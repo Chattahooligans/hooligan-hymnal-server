ea406cc79bb49c90d96f6b016a98c69516038963
John Nelson <john.aaron.nelson@gmail.com>
2018-01-29 15:02:48 -0500

    Getting one song works

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -9,55 +9,58 @@ module.exports = function(app) {
     })
   );
 
+  // Return something for bare URL just so we can be sure the server is running
   app.get('/', function(req, res) {
-    res.send('Chattahooligans API');
+    res.send('Chattahooligans API is running');
   });
 
   // returns all songs
   app.get('/api/songs', function(req, res) {
     Songs.find(function(err, songs) {
-      if (err) throw err;
-      console.log('all songs get');
+      if (err) {
+        console.error(err);
+      }
       res.send(songs);
     });
   });
 
-  // returns single song by id
-  app.get('/api/song/:id', function(req, res) {
-    console.log('id get', req.params.id);
-    Songs.findById(
-      {
-        id: req.params.id
-      },
-      function(err, song) {
-        if (err) throw err;
-        res.send(song);
-      }
-    );
+  // returns single song by guid
+  app.get('/api/song/:guid', function(req, res) {
+    Songs.findOne({ guid: req.params.guid }, 'guid title lyrics').then(song => {
+      res.send(song);
+    });
   });
 
   // creates song
-  app.post('/api/song/:id', function(req, res) {
+  app.post('/api/song', function(req, res) {
     var newSong = Songs(req.body);
     newSong.save(function(err) {
-      if (err) throw err;
+      if (err) {
+        console.error(err);
+      }
       res.send('Successful create');
     });
   });
 
   // updates song
-  app.put('/api/song/:id', function(req, res) {
-    Songs.findByIdAndUpdate(req.params.id, req.body, function(err, song) {
-      if (err) throw err;
+  app.put('/api/song/:guid', function(req, res) {
+    Songs.findOneAndUpdate({ guid: req.params.guid }, req.body, function(
+      err,
+      song
+    ) {
       console.log('song post');
-
+      if (err) {
+        console.error(err);
+      }
       res.send('Successful update');
     });
   });
 
-  app.delete('/api/song/:id', function(req, res) {
-    Songs.findByIdAndRemove(req.params.id, function(err) {
-      if (err) throw err;
+  app.delete('/api/song/:guid', function(req, res) {
+    Songs.findOneAndRemove({ guid: req.params.guid }, function(err) {
+      if (err) {
+        console.error(err);
+      }
       res.send('Successful delete');
     });
   });
--- models/songModel.js
+++ models/songModel.js
@@ -1,9 +1,24 @@
 var mongoose = require('mongoose');
 
 var songSchema = new mongoose.Schema({
+  guid: String,
+  category: String,
+  create_time: String,
+  update_time: String,
   title: String,
-  id: String,
-  lyrics: String
+  tags: String,
+  lyrics: String,
+  tbd_various_boolean_flags: String,
+  reference_title: String,
+  reference_link: String,
+  instructions: String,
+  is_npsl_player: Boolean,
+  is_wpsl_player: Boolean,
+  is_nisa_player: Boolean,
+  player_name: String,
+  player_number: String,
+  override_html: String,
+  delete_local: String
 });
 
 module.exports = mongoose.model('song', songSchema);
