70b8860e7056da168bf61ad0e85edf3c65b1183d
Zachary Parrish <zacharyparrish@protonmail.com>
2018-02-04 15:15:15 -0500

    no longer testing

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ var notificationController = require('./controllers/notificationController');
 var pushTokenController = require('./controllers/pushTokenController');
 
 var port = process.env.PORT || 3000;
-let MONGO_URI_STR = process.env.MONGO_URI;
-let MONGO_URI = 'mongodb://hymnadmin:hooligans@ds151207.mlab.com:51207/chattahooliganhymnal'
-console.log(process.env);
+let MONGO_URI = process.env.MONGO_URI;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect(MONGO_URI_STR, { useMongoClient: true });
+mongoose.connect(MONGO_URI, { useMongoClient: true });
 
 songController(app);
 notificationController(app);
