56de3ef9115f2baa16175b3291e5352561dd35b0
Zachary Parrish <zachary@parrish.io>
2018-10-08 18:29:18 -0400

    Added some headers to prevent CORS with local development using the dev server

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ let MONGO_URI = process.env.MONGO_URI;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.set('view engine', 'ejs');
 
 mongoose.connect(MONGO_URI, { useMongoClient: true });
