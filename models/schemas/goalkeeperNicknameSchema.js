18985189955f71083c73607646da112fb25229d9
Galen Riley <galenriley@gmail.com>
2019-05-10 17:38:31 -0400

    add timestamp to gk nickname

--- models/schemas/goalkeeperNicknameSchema.js
+++ models/schemas/goalkeeperNicknameSchema.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-module.exports = new mongoose.Schema({
-  nickname: String,
-  backgroundColor: String,
-  textColor: String
-});
+module.exports = new mongoose.Schema(
+  {
+    nickname: String,
+    backgroundColor: String,
+    textColor: String
+  },
+  {
+    timestamps: {
+      createdAt: "create_time",
+      updatedAt: "update_time"
+    }
+  }
+);
