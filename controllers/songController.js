c896899bcb745f4cc9169b699ba981bfcd7eaf1c
Sam Shrum <Sam.L.Shrum@gmail.com>
2019-06-07 07:43:09 -0400

    Add caching config var + add caching layer to songs

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,32 @@
 var Songs = require('../models/songs');
 var Notifications = require('../models/notifications');
 var bodyParser = require('body-parser');
+var config = require("../config.js");
+
+var song_cache = {
+  data: null,
+  last_refresh: 0,
+  force_reload: function(res) {
+    var that = this;
+    Songs.find((error, songs) => {
+      if (error) {
+        that.data = {};
+      }
+      that.data = songs;
+      that.last_refresh = Date.now();
+      if(res != null) res.send(that.data);
+    });
+  },
+  send_data: function(res) {
+    if(this.last_refresh + config.cache_timeout < Date.now()) {
+      console.log("forcing reload");
+      this.force_reload(res);
+    } else {
+      console.log("sending from cache");
+      res.send(this.data);
+    }
+  }
+}
 
 module.exports = app => {
   app.use(bodyParser.json());
@@ -17,12 +43,7 @@ module.exports = app => {
 
   // returns all songs
   app.get('/api/songs', (req, res) => {
-    Songs.find((error, songs) => {
-      if (error) {
-        res.status(501).send({ error });
-      }
-      res.send(songs);
-    });
+    song_cache.send_data(res);
   });
 
   // returns single song by _id
@@ -38,6 +59,7 @@ module.exports = app => {
     newSong.save((error, song) => {
       error ? res.status(501).send({ error }) : res.send(song);
     });
+    song_cache.force_reload();
   });
 
   // updates song
@@ -45,6 +67,7 @@ module.exports = app => {
     Songs.findByIdAndUpdate(req.params.id, req.body, (error, song) => {
       error ? res.status(501).send({ error }) : res.send(song);
     });
+    song_cache.force_reload();
   });
 
   // deletes song
@@ -54,5 +77,6 @@ module.exports = app => {
         ? res.status(501).send({ error })
         : res.send({ message: 'Deleted' + req.params.id });
     });
+    song_cache.force_reload();
   });
 };
