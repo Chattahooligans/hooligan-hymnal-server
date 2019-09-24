61d87826934485856bd8070fa2427de3d525d183
Sam Shrum <Sam.L.Shrum@gmail.com>
2019-09-24 19:19:52 -0400

    Handle session timeouts and don't crash if user isn't found

--- models/permissions.js
+++ models/permissions.js
@@ -5,24 +5,40 @@ const keys = ["TEST DON'T USE THIS"];
 
 module.exports.userCanEditRoster = function(req, res, callback) {
     getUser(req, res, (user) => {
+        if(user == null) {
+            callback(false); 
+            return;
+        }
         callback(user.rosterAllowed);
     });
 }
 
 module.exports.userCanPushNotification = function(req, res, callback) {
     getUser(req, res, (user) => {
+        if(user == null) {
+            callback(false); 
+            return;
+        }
         callback(user.pushNotificationsAllowed);
     });
 }
 
 module.exports.userCanEditSongbook = function(req, res, callback) {
     getUser(req, res, (user) => {
+        if(user == null) {
+            callback(false); 
+            return;
+        }
         callback(user.songbookAllowed);
     });
 }
 
 module.exports.userCanEditFoes = function(req, res, callback) {
     getUser(req, res, (user) => {
+        if(user == null) {
+            callback(false); 
+            return;
+        }
         callback(user.foesAllowed);
     });
 }
@@ -33,10 +49,17 @@ function getUser(req, res, callback) {
     Sessions.findOne({sessionKey: sessionKey}, (error, session) => { 
         if(error) {
             callback(null);
+            return;
+        }
+        //null implies no timeout
+        if(session.expiresOn == null || session.expiresOn < new Date()) {
+            callback(null);
+            return;
         }
         Users.findOne({email: session.email}, (err, user) => {
             if(err) {
                 callback(null);
+                return;
             }
             callback(user);
         });
