c424a34b7b6584b48382f84fe9365e7eb7bbae4d
Collin <ococncol@gmail.com>
2019-09-28 12:58:51 -0400

    added refresh endpoint

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,8 @@ const User = require("../models/users");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
+let tokenList = {};
+
 module.exports = app => {
   app.post("/api/users/register", (req, res) => {
     const { email, password } = req.body;
@@ -33,10 +35,23 @@ module.exports = app => {
         if (isMatch) {
           const payload = { id: user.id };
           const secretOrKey = process.env.SECRET_KEY || "NOTsoSECRETkey";
+          const tokenExpires = process.env.TOKEN_EXPIRES || "30m";
+          const refreshSecretOrKey =
+            process.env.REFRESH_SECRET_KEY || "NOTsoSECRETkey";
+          const refreshTokenExpires = process.env.REFRESH_TOKEN_EXPIRES || "1d";
           const token = jwt.sign(payload, secretOrKey, {
-            expiresIn: "2h"
+            expiresIn: tokenExpires
           });
-          res.json({ message: "Logged In", token });
+          const refreshToken = jwt.sign(payload, refreshSecretOrKey, {
+            expiresIn: refreshTokenExpires
+          });
+          const response = {
+            message: "Logged In",
+            token: token,
+            refreshToken: refreshToken
+          };
+          tokenList[refreshToken] = response;
+          res.status(200).json(response);
         } else {
           res.status(401).json({ message: "The password is incorrect" });
         }
@@ -48,14 +63,56 @@ module.exports = app => {
     "/api/users/me",
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
-      res.json({ user: req.user });
+      const {
+        email,
+        foesAllowed,
+        songbookAllowed,
+        rosterAllowed,
+        pushNotificationsAllowed
+      } = req.user;
+      res.json({
+        user: {
+          email,
+          foesAllowed,
+          songbookAllowed,
+          rosterAllowed,
+          pushNotificationsAllowed
+        }
+      });
     }
   );
+
+  app.post("/api/users/refresh", (req, res) => {
+    const { refreshToken } = req.body;
+    if (refreshToken && refreshToken in tokenList) {
+      const refToken = jwt.decode(refreshToken);
+      User.findById(refToken.id, (err, user) => {
+        if (user) {
+          const payload = { id: user.id };
+          const secretOrKey = process.env.SECRET_KEY || "NOTsoSECRETkey";
+          const tokenExpires = process.env.TOKEN_EXPIRES || "30m";
+
+          const token = jwt.sign(payload, secretOrKey, {
+            expiresIn: tokenExpires
+          });
+
+          const response = {
+            token: token
+          };
+          tokenList[refreshToken].token = token;
+          res.status(200).json(response);
+        }
+      });
+    } else {
+      res.status(404).send("Invalid please log back in");
+    }
+  });
 
   app.post(
     "/api/users/logout",
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
+      req.user = {};
       res.status(201).json({ message: "You have been logged out" });
     }
   );
