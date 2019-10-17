12a6107332481b4327cc69b74a4115fa7ee8e6dc
Collin OConnell <ococncol@gmail.com>
2019-10-17 09:18:04 -0400

    updating styling for roster create

--- src/views/rosters/create.vue
+++ src/views/rosters/create.vue
@@ -1,115 +1,150 @@
 <template>
   <Layout>
-    <h2>Create</h2>
-    <!-- <RosterForm
-      :newSquad="newSquad"
-      :newPlayer="newPlayer"
-      :roster="roster"
-      :addRoster="addRoster"
-      :addSquad="addSquad"
-      :removeSquad="removeSquad"
-      :addPlayer="addPlayer"
-      :removePlayer="removePlayer"
-      :selectedSquad="selectedSquad"
-    /> -->
-    <form @submit.prevent="addRoster" method="POST">
-      <div>
-        <label for="rosterTitle">Roster Title</label>
-        <input
-          type="text"
-          name="rosterTitle"
-          id="rosterTitle"
-          v-model="roster.rosterTitle"
-          placeholder="rosterTitle"
-        />
-      </div>
-      <div>
-        <label for="season">Season</label>
-        <input
-          type="text"
-          name="season"
-          id="season"
-          v-model="roster.season"
-          placeholder="Season"
-        />
-      </div>
-      <div v-if="roster.squads.length">
-        <h3>Squads</h3>
-        <div v-for="(squad, index) in roster.squads" :key="index">
-          {{ squad.squadTitle }} -
-          <button type="button" @click="removeSquad(index)">Remove</button>
-          <ul v-if="squad.players">
-            <li v-for="(player, i) in squad.players" :key="i">
-              {{ player.hint }} -
-              <button
-                type="button"
-                @click.prevent="removePlayer(index, player._id)"
-              >
-                Remove
-              </button>
-            </li>
-          </ul>
+    <div class="shadow-xl p-3 mt-3">
+      <h2 class="font-semibold text-2xl mb-3 border-b">Create</h2>
+      <!-- <RosterForm
+        :newSquad="newSquad"
+        :newPlayer="newPlayer"
+        :roster="roster"
+        :addRoster="addRoster"
+        :addSquad="addSquad"
+        :removeSquad="removeSquad"
+        :addPlayer="addPlayer"
+        :removePlayer="removePlayer"
+        :selectedSquad="selectedSquad"
+      /> -->
+      <form @submit.prevent="addRoster" method="POST">
+        <div class="flex flex-col mb-3">
+          <BaseInput
+            label="Roster Title"
+            name="rosterTitle"
+            placeholder="Roster Title"
+            v-model="roster.rosterTitle"
+            type="string"
+            :required="true"
+          />
         </div>
-      </div>
-      <div>
-        <button type="submit">Add Roster</button>
-      </div>
-    </form>
-
-    <form @submit.prevent="addSquad">
-      <div>
-        <label for="squadTitle">Squad Title</label>
-        <input
-          type="text"
-          name="squadTitle"
-          id="squadTitle"
-          v-model="newSquad.squadTitle"
-        />
-      </div>
-      <div>
-        <button type="submit">Add Squad</button>
-      </div>
-    </form>
-
-    <form v-if="roster.squads.length" @submit.prevent="addPlayer">
-      <div>
-        <label for="squad">Select Squad</label>
-        <select name="squad" id="squad" v-model="selectedSquad">
-          <option
+        <div class="flex flex-col mb-3">
+          <BaseInput
+            name="season"
+            label="Season"
+            type="text"
+            placeholder="Season"
+            v-model="roster.season"
+          />
+        </div>
+        <div v-if="roster.squads.length">
+          <h3 class="font-semibold text-xl mb-3 border-b">Squads</h3>
+          <div
+            class="mb-3"
             v-for="(squad, index) in roster.squads"
             :key="index"
-            :value="index"
           >
-            {{ squad.squadTitle }}
-          </option>
-        </select>
-      </div>
-      <div>
-        <label for="player_Id">Player ID</label>
-        <input
-          type="text"
-          name="player_id"
-          id="player_id"
-          v-model="newPlayer._id"
-        />
-      </div>
-      <div>
-        <label for="player_hint">Hint</label>
-        <input
-          type="text"
-          name="player_hint"
-          id="player_hint"
-          v-model="newPlayer.hint"
-        />
-      </div>
-      <div>
-        <button type="submit">Add Player</button>
-      </div>
-    </form>
+            <h3 class="text-xl inline-block">{{ squad.squadTitle }}</h3>
+            -
+            <button
+              type="button"
+              class="p-1 px-2 rounded bg-red-600 text-white"
+              @click="removeSquad(index)"
+            >
+              Remove
+            </button>
+            <ul v-if="squad.players">
+              <li v-for="(player, i) in squad.players" :key="i">
+                {{ player.hint }} -
+                <button
+                  type="button"
+                  @click.prevent="removePlayer(index, player._id)"
+                >
+                  Remove
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="mb-3">
+          <button
+            class="px-3 py-2 rounded bg-blue-600 text-white"
+            type="submit"
+          >
+            Add Roster
+          </button>
+        </div>
+      </form>
+
+      <form @submit.prevent="addSquad">
+        <div class="flex flex-col mb-3">
+          <BaseInput
+            name="squadTitle"
+            type="text"
+            label="Squad Title"
+            placeholder="Squad Title"
+            v-model="newSquad.squadTitle"
+            :required="true"
+          />
+        </div>
+        <div class="mb-3">
+          <button
+            class="px-3 py-2 rounded bg-green-600 text-white"
+            type="submit"
+          >
+            Add Squad
+          </button>
+        </div>
+      </form>
+
+      <form v-if="roster.squads.length" @submit.prevent="addPlayer">
+        <h3 class="font-smaller font-semibold mb-2">Add Player</h3>
+        <div class="flex flex-col mb-3">
+          <label class="font-dark font-lg mb-2" for="squad">Select Squad</label>
+          <select
+            class="p-6 bg-white border h-10"
+            name="squad"
+            id="squad"
+            v-model="selectedSquad"
+          >
+            <option
+              class="h-10"
+              v-for="(squad, index) in roster.squads"
+              :key="index"
+              :value="index"
+            >
+              {{ squad.squadTitle }}
+            </option>
+          </select>
+        </div>
+        <div class="flex flex-col mb-3">
+          <BaseInput
+            type="text"
+            label="Player ID"
+            placeholder="Player ID"
+            name="player_id"
+            :required="true"
+            v-model="newPlayer._id"
+          />
+        </div>
+        <div class="flex flex-col mb-3">
+          <BaseInput
+            type="text"
+            label="Player Name"
+            name="player_hint"
+            :required="true"
+            placeholder="Player Name"
+            v-model="newPlayer.hint"
+          />
+        </div>
+        <div>
+          <button class="px-3 py-2 bg-blue-600 text-white" type="submit">
+            Add Player
+          </button>
+        </div>
+      </form>
+    </div>
   </Layout>
 </template>
 
 <script>
+import BaseInput from "@/components/BaseInput";
 import axios from "axios";
 export default {
   data() {
@@ -130,7 +165,9 @@ export default {
       selectedSquad: ""
     };
   },
-  components: {},
+  components: {
+    BaseInput
+  },
   methods: {
     addRoster() {
       axios
@@ -160,9 +197,6 @@ export default {
       this.roster.squads[this.selectedSquad].players = this.roster.squads[
         this.selectedSquad
       ].players.concat(this.newPlayer);
-      // this.roster.squads[this.selectedSquad].players = this.roster.squads[
-      //   this.selectedSquad
-      // ].players.concat(this.newPlayer);
       this.selectedSquad = null;
       this.newPlayer = {
         _id: "",
@@ -170,9 +204,9 @@ export default {
       };
     },
     removePlayer(index, id) {
-      this.squads[index].players = this.squads[index].players.filter(
-        player => player._id !== id
-      );
+      this.roster.squads[index].players = this.roster.squads[
+        index
+      ].players.filter(player => player._id !== id);
     }
   }
 };
