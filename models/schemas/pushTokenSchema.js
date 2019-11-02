40448b2c6e8ac27b7517112dae6914f8f309c364
Galen Riley <galenriley@gmail.com>
2019-11-02 18:21:48 -0400

    add appVersion to push token schema

--- models/schemas/pushTokenSchema.js
+++ models/schemas/pushTokenSchema.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = new mongoose.Schema({
   pushToken: String,
   expoExperience: String,
+  appVersion: String,
   lastUsed: String,
   platform: String,
   platformVersion: String,
