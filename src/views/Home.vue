708807dc6711a7cde95125d7cbfe99b96a9b7034
Collin OConnell <ococncol@gmail.com>
2019-11-05 09:26:31 -0500

    cache busting work?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="text-center container mx-auto py-3">
-      <h1 class="font-bold text-3xl">Welcome to Hymnal Server</h1>
+      <h1 class="font-bold text-3xl">Welcome to Hymnal Server!</h1>
       <fragment v-if="loggedIn">
         <p>If you see nothing on the side please contact your admin.</p>
       </fragment>
