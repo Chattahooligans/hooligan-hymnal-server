b2d71325949b29e8eedd404d424d51bf260c7fde
Galen Riley <galenriley@gmail.com>
2019-11-06 15:38:43 -0500

    add .voice, images.credit

--- models/schemas/feedItemSchema.js
+++ models/schemas/feedItemSchema.js
@@ -4,12 +4,13 @@ var ObjectId = mongoose.Schema.Types.ObjectId
 var feedItem = new Schema(
     {
         sender: { user: ObjectId, pushToken: String },
+        voice: ObjectId,
         publishAt: Date,
         unpublishAt: Date,
         pinnedAt: Date,
         push: Boolean,
-        text: String || { locale: String, text: String },
-        images: [{url: String}],
+        text: { locale: String, text: String },
+        images: [{url: String, credit: String, default: Boolean}],
         attachments: [{type: String, id: ObjectId, data: null}],
         active: Boolean
     },
