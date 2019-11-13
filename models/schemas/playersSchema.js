f8649f1c5d88b0a36c5df0e2a6e7a759507ea590
Collin <ococncol@gmail.com>
2019-11-12 23:04:11 -0500

    added commented out bio example

--- models/schemas/playersSchema.js
+++ models/schemas/playersSchema.js
@@ -1,20 +1,28 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-module.exports = new mongoose.Schema({
-  name: String,
-  flag: String,
-  squadNumber: String,
-  position: String,
-  team: String,
-  bio: String,
-  thumbnail: String,
-  image: String,
-  twitter: String,
-  instagram: String
-}, {
-  timestamps: {
-    createdAt: 'create_time',
-    updatedAt: 'update_time'
+module.exports = new mongoose.Schema(
+  {
+    name: String,
+    flag: String,
+    squadNumber: String,
+    position: String,
+    team: String,
+    bio: String,
+    // TODO: WIll update this in the morning.
+    // bio: {
+    //   type: Map,
+    //   of: String
+    // },
+    thumbnail: String,
+    image: String,
+    twitter: String,
+    instagram: String
+  },
+  {
+    timestamps: {
+      createdAt: "create_time",
+      updatedAt: "update_time"
+    }
   }
-});
+);
