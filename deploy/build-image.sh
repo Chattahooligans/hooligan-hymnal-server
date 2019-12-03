15ad8da8a1ab983934b240c2c0338be75b8d0b97
Sam Shrum <Sam.L.Shrum@gmail.com>
2019-12-02 19:05:37 -0500

    Adding packer file betas

---
+++ deploy/build-image.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#run this in order to prep everything and run Packer to create an image in GCP.
+#note that this is experimental and would probably not be suitable for production.
+#please see the Hooligan Hymnal docs for information on deploying to Heroku instead.
+
+#get script's directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd DIR/..
+zip -r /tmp/build.zip .
+packer build packer.json
---
+++ deploy/remote-provision.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs zip unzip nginx build-essential
+sudo npm install -g pm2
+mkdir app
+mv /tmp/build.zip ~/app/build.zip
+cd app
+unzip build.zip
+rm build.zip
+#add app.js to pm2 so it manages it
+pm2 start app.js
+#output from this configures pm2 to run at startup
+pm2 startup systemd | tail -1 | source /dev/stdin
+mv /tmp/nginx-sites-available.conf /etc/nginx/sites-available/default
+sudo systemctl restart nginx
