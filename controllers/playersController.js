ad6f2f0dab5c88c2dd6d71397f883fcd30ee8b3a
Collin <ococncol@gmail.com>
2019-12-12 23:46:12 -0500

    starting crud for players

---
+++ controllers/playersController.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Player = mongoose.model("players");
+
+exports.index = async (req, res) => {};
+exports.create = (req, res) => {};
+exports.store = async (req, res) => {};
+exports.show = async (req, res) => {};
+exports.edit = async (req, res) => {};
+exports.update = async (req, res) => {};
+exports.deleteConfirm = async (req, res) => {};
+exports.delete = async (req, res) => {};
