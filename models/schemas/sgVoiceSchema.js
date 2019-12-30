3634c9ed8ecd595fa827765fd0b00d204bcf6bf2
Galen Riley <galenriley@gmail.com>
2019-12-30 15:34:53 -0500

    remove _id declaration

--- models/schemas/sgVoiceSchema.js
+++ models/schemas/sgVoiceSchema.js
@@ -1,9 +1,14 @@
 var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+// Note to self: https://stackoverflow.com/questions/45952928/mongodb-error-document-must-have-an-id-before-saving
+/*
+If you have declared _id field explicitly in schema, you must initialize it explicitly
+If you have not declared it in schema, MongoDB will declare and initialize it.
+*/
+
 module.exports = new mongoose.Schema(
     {
-        _id: ObjectId,
         name: String,
         defaultLocale: String,
         description: String,
