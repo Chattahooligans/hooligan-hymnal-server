fbe1a07ec93af3f9eea9d3d6fa22633bfa9bec2d
Sam Shrum <Sam.L.Shrum@gmail.com>
2020-01-05 15:02:04 -0500

    Add some cache functions and endpoints for feed

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,4 +1,5 @@
-const feeditems = require("../models/feeditems");
+const FeedItems = require("../models/feeditems");
+const Channels = require("../models/channels");
 const config = require("../config.js");
 const passport = require("passport");
 const permissions = require("../middleware/PermissionsMiddleware");
@@ -6,9 +7,9 @@ const permissions = require("../middleware/PermissionsMiddleware");
 var feeditems_cache = {
   data: null,
   last_refresh: 0,
-  force_reload: function (res) {
+  force_reload: function (res, sendCallback) {
     var that = this;
-    feeditems.find((error, feed) => {
+    FeedItems.find((error, feed) => {
       if (error) {
         that.data = null;
         that.last_refresh = 0;
@@ -16,39 +17,91 @@ var feeditems_cache = {
       }
       that.data = feed;
       that.last_refresh = Date.now();
-      if (res != null) res.send(that.data);
+      if (res != null) {
+        sendCallback(that.data);
+      }
     });
   },
   send_data: function (res) {
     if (this.last_refresh + config.cache_timeout < Date.now()) {
-      this.force_reload(res);
+      this.force_reload(res, res.send);
     } else {
       res.send(this.data);
     }
+  },
+  send_active: function(res) {
+    if (this.last_refresh + config.cache_timeout < Date.now()) {
+      this.force_reload(res, (data) => res.send(this.get_active_items(data)));
+    } else {
+      res.send(this.get_active_items(this.data));
+    }
+  },
+  send_channel: function(res, channelId) {
+    if (this.last_refresh + config.cache_timeout < Date.now()) {
+      this.force_reload(res, (data) => res.send(this.get_channel_items(data, channelId)));
+    } else {
+      res.send(this.get_channel_items(this.data, channelId));
+    }
+  },
+  get_active_items(data) {
+    var active = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].active) active.push(data[i]);
+    }
+    return active;
+  },
+  get_channel_items(data, channelId) {
+    var items = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].channel == channelId && data[i].channel) items.push(data[i]);
+    }
+    return items;
   }
 };
 
 module.exports = app => {
   app.get("/api/feed", (req, res) => {
     feeditems_cache.send_data(res);
-    // TODO: only return where .active=true
     // And where the associated channel .active=true
   });
 
-  // TODO: get /api/feed/ and pass it a post _id
+  app.get("/api/feed/active", (req, res) => {
+    feeditems_cache.send_active(res);
+    // And where the associated channel .active=true
+  });
 
-  // TODO: get /api/feed/channel (or something) and pass it a channel _id
+  // returns single item by _id
+  app.get("/api/feed/:id", (req, res) => {
+    FeedItems.findById(req.params.id, (error, feedItem) => {
+      res.send(feedItem);
+    });
+  });
 
-  // TODO: require admin user credentials, feedAllowed permission, and channel.user.canCreate
+  app.get("/api/feed/channel/:id", (req, res) => {
+    feeditems_cache.send_channel(res, req.params.id);
+  });
+
   // creates feed item
   app.post(
     "/api/feed",
+    passport.authenticate("jwt", { session: false }),
+    permissions("feedAllowed"),
     (req, res) => {
-      var feedItem = feeditems(req.body);
-      feedItem.save((error, item) => {
-        error ? res.status(501).send({ error }) : res.send(item);
-        feeditems_cache.force_reload();
-      });
+      var feedItem = FeedItems(req.body);
+      Channels.findById(feedItem.channel.Id), (error, channel) => {
+        if(error) {
+          res.send(error);
+        }
+        //TODO: need to get currentUserId for comparison. possibly ask Passport?
+        var userHasPermission = channel.users.some((user) => user.canCreate && user._id === req.user._id);
+        if(!userHasPermission) {
+          res.status(401).send("You do not have permission to post to this news feed channel!");
+        }
+        feedItem.save((error, item) => {
+          error ? res.status(501).send({ error }) : res.send(item);
+          feeditems_cache.force_reload();
+        });
+      }
     }
   );
 
