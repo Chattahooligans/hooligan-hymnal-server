ddd9db42e1dbdc33527735614bc4a944443ae340
Collin <ococncol@gmail.com>
2020-01-11 15:43:24 -0500

    removed commented out files

--- src/js/app.js
+++ src/js/app.js
@@ -138,38 +138,3 @@ dropzone(
   1,
   "thumbnail"
 )
-
-// dropzone(
-//   "/players/thumbnail",
-//   "thumbnail-template",
-//   document.getElementById("thumbnail-upload-section"),
-//   "#thumbnail-previews",
-//   "#thumbnail-target",
-//   "Thumbnail",
-//   1
-// )
-
-// var previewNode = document.querySelector("#template");
-// previewNode.id = "";
-// var previewTemplate = previewNode.parentNode.innerHTML;
-// var myDropzone = new Dropzone(document.getElementById("upload-section"), {
-//   url: "/players/thumbnail",
-//   previewTemplate: previewTemplate,
-//   autoQueue: false,
-//   previewsContainer: "#previews",
-// })
-
-// document.querySelector("#actions .upload").onclick = function () {
-//   myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-// }
-// myDropzone.on("success", (_, res) => {
-//   console.log(res);
-//   var target = document.querySelector("#target");
-//   var input = document.createElement("input");
-//   input.value = res.url;
-//   input.setAttribute("data-id", res.id);
-//   input.classList.add = "block";
-//   input.setAttribute("name", "images[]");
-//   target.appendChild(input);
-//   // console.log(res);
-// })
