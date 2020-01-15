f345886b1fd51d6109e29affac0f091d72fe804a
Ross Singer <rossfsinger@gmail.com>
2020-01-15 15:36:06 -0500

    Shield ourselves from the config

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const env = require("dotenv").config();
-var config = {};
+var config = {CLOUDINARY_API_KEY: null, CLOUDINARY_CLOUDNAME: null};
 
 config.cache_timeout = 15 * 60 * 1000; //15 minutes
 const CLOUDINARY_URL = process.env.CLOUDINARY_URL;
-config.CLOUDINARY_API_KEY = CLOUDINARY_URL.split(":")[1].replace("//", "");
-config.CLOUDINARY_CLOUDNAME = CLOUDINARY_URL.split("@")[1];
 
+if (CLOUDINARY_URL) {
+    config.CLOUDINARY_API_KEY = CLOUDINARY_URL.split(":")[1].replace("//", "");
+    config.CLOUDINARY_CLOUDNAME = CLOUDINARY_URL.split("@")[1];
+}
 module.exports = config;
