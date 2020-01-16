6b0d61505fdd183edee74d3e53dfb6c3d2820f76
Collin <ococncol@gmail.com>
2020-01-15 23:21:20 -0500

    feedController

--- controllers/API/feedController.js
+++ controllers/API/feedController.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
-const FeedItem = mongoose.model("FeedItem");
-const Channel = mongoose.model("Channel");
-const config = require("../../config");
-// Is this required??
-// const PushHandler = mongoose.model("PushHandler");
+const FeedItems = mongoose.model("feedItem");
+const Channels = mongoose.model("channels");
+const config = require("../config");
+const { checkPermission } = require("../../middleware/PermissionsMiddleware");
+// let PushHandler = require("../../models/pushHandler")
 
-let feeditems_cache = {
+const feeditems_cache = {
 	data: null,
 	last_refresh: 0,
-	force_reload: function (res, sendCallback) {
-		var that = this;
+	force_reload: function(res, sendCallback) {
+		const that = this;
 		FeedItems.find((error, feed) => {
 			if (error) {
 				that.data = null;
@@ -23,44 +23,48 @@ let feeditems_cache = {
 			}
 		});
 	},
-	send_data: function (res) {
+	send_data: function(res) {
 		if (this.last_refresh + config.cache_timeout < Date.now()) {
 			this.force_reload(res, res.send);
 		} else {
-			res.json(this.data);
+			res.send(this.data);
 		}
 	},
-	send_active: function (res) {
+	send_active: function(res) {
 		if (this.last_refresh + config.cache_timeout < Date.now()) {
-			this.force_reload(res, (data) => res.send(this.get_active_items(data)));
+			this.force_reload(res, data => res.send(this.get_active_items(data)));
 		} else {
-			res.json(this.get_active_items(this.data));
+			res.send(this.get_active_items(this.data));
 		}
 	},
-	send_channel: function (res, channelId) {
+	send_channel: function(res, channelId) {
 		if (this.last_refresh + config.cache_timeout < Date.now()) {
-			this.force_reload(res, (data) => res.send(this.get_channel_items(data, channelId)));
+			this.force_reload(res, data => res.send(this.get_channel_items(data, channelId)));
 		} else {
-			res.json(this.get_channel_items(this.data, channelId));
+			res.send(this.get_channel_items(this.data, channelId));
 		}
 	},
 	get_active_items(data) {
-		var active = [];
-		for (let i = 0; i < data.length; i++) {
-			if (data[i].active) active.push(data[i]);
+		let active = [];
+		for(let i = 0; i < data.length; i++) {
+			if (data[i].active) {
+				active.push(data[i]);
+			}
 		}
 		return active;
 	},
 	get_channel_items(data, channelId) {
-		var items = [];
+		let items = [];
 		for (let i = 0; i < data.length; i++) {
-			if (data[i].channel == channelId && data[i].channel) items.push(data[i]);
+			if (data[i].channel == channelId && data[i].channel) {
+				items.push(data[i]);
+			}
 		}
 		return items;
 	}
 };
 
-exports.activeFeed = (req, res) => {
+exports.index = (req, res) => {
 	feeditems_cache.send_active(res);
 };
 
@@ -68,46 +72,90 @@ exports.all = (req, res) => {
 	feeditems_cache.send_data(res);
 };
 
-exports.show = async (req, res) => {
-	const feedItem = await FeedItem.findById(req.params.id);
-	res.json(feedItem);
-};
-
-exports.store = async (req, res, next) => {
-	let feedItem = new FeedItem(req.body);
-	const channel = await Channel.findById(feedItem.channel);
-	let err = {};
-	if (!channel) {
-		err = new Error("No channel found with matching id");
-		return next(err);
-	}
-	if(channel.users.some(user => user.canCreate && user._id === req.user._id)) {
-		err = new Error("You do not have permission to post to this news feed channel!");
-		err.status = 401;
-		return next(err);
-	}
-	await feedItem.save();
-	if (feedItem.push) {
-		await PushHandler.sendPost(notification);
-	}
-	feeditems_cache.force_reload();
-};
-
-exports.destroy = async (req, res, next) => {
-	const feedItem = await FeedItem.findById(req.params.id);
-	const channel = await Channel.findById(feedItem.channel.id);
-	let err = {};
-	if (!channel) {
-		err = new Error("No channel found with that id");
-		return next(err);
-	}
-	if (!channel.users.some(user => user.canDelete && user._id === req.user._id)) {
-		err = new Error("You do not have permission to delete from this news feed channel");
-		err.status = 401;
-		return next(err);
-	}
-	feedItem.remove();
-	res.json({
-		message: `Deleted ${feedItem._id}`
+exports.show = (req, res, next) => {
+	FeedItems.findById(req.params.id, (err, feedItem) => {
+		if (err) {
+			return next(err);
+		}
+		res.json(feedItem);
 	});
 };
+
+exports.channelId = (req, res) => {
+	feeditems_cache.send_channel(res, req.params.id);
+};
+
+exports.store = (req, res, next) => {
+	let feedItem = new FeedItems(req.body);
+	Channels.findById(feedItem.channel, (error, channel) => {
+		if (error) {
+			return next(error);
+		}
+		if (!channel.users.some(user => user.canCreate && String(user._id) == String(req.user._id))) {
+			let err = {};
+			err = new Error("You do not have permission to post to this news feed channel");
+			err.status = 401;
+			return next(err);
+		}
+		feedItem.save((error, item) => {
+			if (error) {
+				return next(error);
+			}
+			if (feedItem.push) {
+				// Send a push notification here
+				// need to translate feedItem into a Notification Object first
+				// TODO currently, this means that Notification form will be sent back, not the feedItem.
+				PushHandler.sendPost(notification)
+					.then(function(results) {
+						results.notification = notification;
+						res.json(results);
+					}).catch(function(error) {
+						// TODO: return an error would be cleaner
+						console.log(`error 2: ${error}`);
+						let err = {};
+						err = new Error(`Error fetching push tokens: ${error}`);
+						err.status = 501;
+						return next(err);
+					});
+			}
+			feeditems_cache.force_reload();
+		});
+	});
+};
+
+exports.destroy = (req, res, next) => {
+	FeedItems.findById(req.params.id, (error, feedItem) => {
+		if (error) {
+			let err = {};
+			err = new Error(error);
+			err.status = 501;
+			return next(err);
+		}
+		Channels.findById(feedItem.channel, (error, channel) => {
+			if (error) {
+				let err = {};
+				err = error;
+				return next(err);
+			}
+			if (!channel.users.some(user => user.canDelete && user._id === req.user._id)) {
+				let err = {};
+				err = new Error("You do not have permission to delete from this news feed channel!");
+				err.status = 401;
+				return next(err);
+			}
+			FeedItems.findById(req.params.id, error => {
+				if (error) {
+					let err = {};
+					err = error;
+					err.status = 501;
+					return next(err);
+				}
+				res.json({
+					message: `Deleted ${req.params.id}`
+				});
+			});
+		});
+	});
+};
+
+// TODO: Image upload to cloudinary
--- middleware/PermissionsMiddleware.js
+++ middleware/PermissionsMiddleware.js
@@ -1,22 +1,22 @@
 exports.apiCheckPermission = function(option) {
-  return (req, res, next) => {
-    const { user } = req;
-    if (user && option in user) {
-      if (user[option]) {
-        next();
-      } else {
-        res.send({ message: "You do not have the correct permissions" });
-      }
-    }
-  };
+	return (req, res, next) => {
+		const { user } = req;
+		if (user && option in user) {
+			if (user[option]) {
+				next();
+			} else {
+				res.send({ message: "You do not have the correct permissions" });
+			}
+		}
+	};
 };
 
 exports.checkPermission = option => {
-  return (req, res, next) => {
-    if (req.user[option]) {
-      return next();
-    }
-    req.flash("info", `You do not have the correct rights to view ${req.path}`);
-    res.redirect("/");
-  };
+	return (req, res, next) => {
+		if (req.user[option]) {
+			return next();
+		}
+		req.flash("info", `You do not have the correct rights to view ${req.path}`);
+		res.redirect("/");
+	};
 };
