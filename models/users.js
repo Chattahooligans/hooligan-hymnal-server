91a0e92d50ac19bd2bbd1bfb73ef02a64c60787e
Collin <ococncol@gmail.com>
2020-01-16 22:23:19 -0500

    fixed user schema issue

--- models/users.js
+++ models/users.js
@@ -1,79 +1,9 @@
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const UserSchema = require("./schemas/userSchema");
-// var userSchema = require("./schemas/userSchema");
-// const Schema = mongoose.Schema;
 const bcryptjs = require("bcrypt");
 const md5 = require("md5");
 const mongodbErrorHandler = require("mongoose-mongodb-errors");
-const passportLocalMongoose = require("passport-local-mongoose");
-
-// const UserSchema = new Schema(
-// 	{
-// 		email: {
-// 			type: String,
-// 			lowercase: true,
-// 			trim: true,
-// 			index: {
-// 				unique: true
-// 			}
-// 		},
-// 		name: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		familyName: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		displayName: {
-// 			type: String,
-// 			required: true,
-// 			index: {
-// 				unique: true
-// 			}
-// 		},
-// 		pushNotificationsAllowed: {
-// 			type: Boolean,
-// 			default: false
-// 		},
-// 		rosterAllowed: {
-// 			type: Boolean,
-// 			default: false
-// 		},
-// 		songbookAllowed: {
-// 			type: Boolean,
-// 			default: false
-// 		},
-// 		foesAllowed: {
-// 			type: Boolean,
-// 			default: false
-// 		},
-// 		feedAllowed: {
-// 			type: Boolean,
-// 			default: false,
-// 		},
-// 		usersAllowed: {
-// 			type: Boolean,
-// 			default: false
-// 		},
-// 		lastLogin: {
-// 			type: Date,
-// 			default: null
-// 		},
-// 		password: String,
-// 		resetPasswordToken: String,
-// 		resetPasswordExpires: Date
-// 	},
-// 	{
-// 		toJSON: {
-// 			virtuals: true
-// 		},
-// 		toObject: {
-// 			virtuals: true
-// 		}
-// 	}
-// );
 
 UserSchema.virtual("gravatar").get(function() {
 	const hash = md5(this.email);
