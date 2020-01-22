390bd711e17d7d8606a45768653b0b3427a67141
Collin <ococncol@gmail.com>
2020-01-22 17:32:27 -0500

    added a little check to see if user already submitted password reset to limit requests to password reset and not spam the account

--- controllers/forgotController.js
+++ controllers/forgotController.js
@@ -13,13 +13,18 @@ exports.forgot = async (req, res) => {
 			gmail_remove_dots: false,
 			gmail_remove_subaddress: false
 		});
+	const timeOffset = 3600000;
 	const user = await User.findOne({ email: req.body.email });
 	if (!user) {
 		req.flash("error", "No account with that email exists!");
 		return res.redirect("/login");
 	}
+	if (user.resetPasswordExpires < Date.now() + timeOffset) {
+		req.flash("error", "You already asked for a reset email this hour. Please check your spam folder if you can not find it.");
+		return res.redirect("/login");
+	}
 	user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
-	user.resetPasswordExpires = Date.now() + 3600000;
+	user.resetPasswordExpires = Date.now() + timeOffset;
 	await user.save();
 	const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
 	await mail.send({
