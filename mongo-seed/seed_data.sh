958fe00269b3f5cd3838e65bbf0c969efb6c42e0
Ross Singer <rossfsinger@gmail.com>
2020-01-26 11:22:29 -0500

    Use correct collection names

--- mongo-seed/seed_data.sh
+++ mongo-seed/seed_data.sh
@@ -3,6 +3,6 @@
 mongoimport --host mongo --db hymnal --collection songs /data/songs.json
 mongoimport --host mongo --db hymnal --collection players /data/players.json
 mongoimport --host mongo --db hymnal --collection users /data/users.json
-mongoimport --host mongo --db hymnal --collection pushTokens /data/pushTokens.json
-mongoimport --host mongo --db hymnal --collection feedItems /data/feedItems.json
+mongoimport --host mongo --db hymnal --collection pushtokens /data/pushTokens.json
+mongoimport --host mongo --db hymnal --collection feeditems /data/feedItems.json
 # Add any other collections here
