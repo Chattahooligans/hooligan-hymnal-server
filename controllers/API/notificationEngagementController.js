c1144b0fd665a6590ca796613dbfe3841bc8ca6b
Ross Singer <rossfsinger@gmail.com>
2020-01-26 12:28:59 -0500

    Summarize engagement; make sure only one engagement exists for device for notification

--- controllers/API/notificationEngagementController.js
+++ controllers/API/notificationEngagementController.js
@@ -3,25 +3,43 @@ const NotificationEngagements = mongoose.model("notificationEngagements");
 const PushTokens = mongoose.model("pushTokens");
 const FeedItems = mongoose.model("feedItem");
 
-exports.create = async (req, res) => {
-	let feedItem = await FeedItems.findById(req.params.id);
-	if (!feedItem) {
-		res.status(404);
-		return;
+exports.create = async (req, res, next) => {
+	try {
+		let feedItem = await FeedItems.findById(req.params.id);
+		if (!feedItem) {
+			res.status(404);
+			res.send("");
+			return;
+		}
+		let pushToken = await PushTokens.findOne({ pushToken: req.body.pushToken });
+		if (!pushToken) {
+			res.status(401);
+			res.send("");
+			return;
+		}
+		let engagementDetails = {
+			feedItem: feedItem._id,
+			pushToken: pushToken.pushToken,
+			// engagedAt: Date.parse(req.body.timestamp)
+		};
+		NotificationEngagements.find(engagementDetails)
+			.then(engagements => {
+				if (engagements.length > 0) {
+					res.status(400);
+					res.send("");
+					return;
+				}
+				engagementDetails.engagedAt = Date.parse(req.body.timestamp);
+				return NotificationEngagements.create(engagementDetails);
+			})
+			.then(() => {
+				res.status(204);
+				res.send("");
+				return;
+			});
+	} catch (error) {
+		return next(error);
 	}
-	let pushToken = await PushTokens.findOne({ pushToken: req.body.pushToken });
-	if (!pushToken) {
-		res.status(401);
-		return;
-	}
-	let engagementDetails = {
-		feedItem: feedItem._id,
-		pushToken: pushToken.pushToken,
-		engagedAt: Date.parse(req.body.timestamp)
-	};
-	let engagement = NotificationEngagements.create(engagementDetails);
-	engagement.save();
-	res.status(204);
 };
 
 exports.show = async (req, res, next) => {
@@ -33,23 +51,42 @@ exports.show = async (req, res, next) => {
 			res.send("");
 			return;
 		}
-		let engagements = NotificationEngagements.find({feedItem: feedItem._id});
-		res.send(engagements);
+		NotificationEngagements.find({feedItem: feedItem._id})
+			.then(engagements => {
+				res.send(engagements);
+				return;
+			});
+
 	} catch (error) {
 		return next(error);
 	}
 };
 
-exports.summarize = async (req, res) => {
-	let feedItem = await FeedItems.findById(req.params.id);
-	console.log(feedItem);
-	if (!feedItem) {
-		res.status(404);
-		return;
+exports.summarize = async (req, res, next) => {
+	try {
+		let feedItem = await FeedItems.findById(req.params.id);
+		if (!feedItem) {
+			res.status(404);
+			res.send("");
+			return;
+		}
+		NotificationEngagements.aggregate([
+			{ $match: { feedItem: feedItem._id}},
+			{ $group: {
+				_id: feedItem._id,
+				count: { $sum: 1 } ,
+				"firstEngagement": {$min: "$createdAt"},
+				"latestEngagement": {$max: "$createdAt"},
+			}}
+		])
+			.then(engagements => {
+				res.send(engagements);
+				return;
+			})
+			.catch(error => {
+				return next(error);
+			});
+	} catch (error) {
+		return next(error);
 	}
-	let engagements = NotificationEngagements.aggregate()
-		.match({ feedItem: feedItem._id })
-		.count("_id")
-		.exec();
-	res.send(engagements);
 };
