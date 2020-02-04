e8ef12df4fe25bf8cc06031866ec0739b9d93f19
Collin OConnell <ococncol@gmail.com>
2020-02-04 15:42:34 -0500

    working on adding images to posts

--- controllers/API/feedController.js
+++ controllers/API/feedController.js
@@ -1,64 +1,68 @@
-const FeedItems = require("../../models/feeditems");
-const Channels = require("../../models/channels");
-const config = require("../../config.js");
-let PushHandler = require("../../models/pushHandler");
+const FeedItems = require('../../models/feeditems');
+const Channels = require('../../models/channels');
+const config = require('../../config.js');
+const PushHandler = require('../../models/pushHandler');
+const { upload } = require('../../handlers/imageUploader');
 
-var feeditems_cache = {
+const feeditems_cache = {
   data: null,
   last_refresh: 0,
-  force_reload: function (res, sendCallback) {
-    var that = this;
+  force_reload(res, sendCallback) {
+    const that = this;
     FeedItems.find((error, feed) => {
       if (error) {
         that.data = null;
         that.last_refresh = 0;
         if (res != null) res.send(error);
       }
-      //sort cache on publishedAt, descending
-      that.data = feed.sort((a, b) => a.publishedAt < b.publishedAt ? 1 : -1);
+      // sort cache on publishedAt, descending
+      that.data = feed.sort((a, b) => (a.publishedAt < b.publishedAt ? 1 : -1));
       that.last_refresh = Date.now();
       if (res != null) {
         sendCallback(that.data);
       }
     });
   },
-  send_data: function (res, publishedBefore, limit) {
+  send_data(res, publishedBefore, limit) {
     if (this.last_refresh + config.cache_timeout < Date.now()) {
       this.force_reload(res, (data) => res.send(
-        this.filter_data(data, publishedBefore, limit)));
+        this.filter_data(data, publishedBefore, limit),
+      ));
     } else {
       res.send(this.filter_data(this.data, publishedBefore, limit));
     }
   },
-  send_active: function(res, publishedBefore, limit) {
+  send_active(res, publishedBefore, limit) {
     if (this.last_refresh + config.cache_timeout < Date.now()) {
       this.force_reload(res, (data) => res.send(
         this.filter_data(
-          this.get_active_items(data), publishedBefore, limit)
-          ));
+          this.get_active_items(data), publishedBefore, limit,
+        ),
+      ));
     } else {
       res.send(this.filter_data(this.get_active_items(this.data), publishedBefore, limit));
     }
   },
-  send_channel: function(res, channelId, publishedBefore, limit) {
+  send_channel(res, channelId, publishedBefore, limit) {
     if (this.last_refresh + config.cache_timeout < Date.now()) {
       this.force_reload(res, (data) => res.send(
         this.filter_data(
-          this.get_channel_items(data, channelId), publishedBefore, limit)
-          ));
+          this.get_channel_items(data, channelId), publishedBefore, limit,
+        ),
+      ));
     } else {
       res.send(this.filter_data(this.get_channel_items(this.data, channelId), publishedBefore, limit));
     }
   },
   get_active_items(data) {
-    var active = [];
+    const active = [];
     for (let i = 0; i < data.length; i++) {
       if (data[i].active) active.push(data[i]);
     }
     return active;
   },
   get_channel_items(data, channelId) {
-    var items = [];
+    const items = [];
     for (let i = 0; i < data.length; i++) {
       if (data[i].channel == channelId && data[i].channel) items.push(data[i]);
     }
@@ -66,19 +70,19 @@ var feeditems_cache = {
   },
   filter_data(data, publishedBefore, limit) {
     limit = parseInt(limit);
-    publishedBefore = Date.parse(publishedBefore);    console.log(publishedBefore);
+    publishedBefore = Date.parse(publishedBefore); console.log(publishedBefore);
     if (!publishedBefore) publishedBefore = new Date();
-    if(!limit) limit = 20;
-    var filtered = data.filter(i => i.publishedAt < publishedBefore);
+    if (!limit) limit = 20;
+    const filtered = data.filter((i) => i.publishedAt < publishedBefore);
     return filtered.slice(0, limit);
-  }
+  },
 };
 
 exports.active = async (req, res) => {
   feeditems_cache.send_active(res, req.query.publishedBefore, req.query.limit);
 };
 
-exports.all = async(req, res) => {
+exports.all = async (req, res) => {
   feeditems_cache.send_data(res, req.query.publishedBefore, req.query.limit);
 };
 
@@ -93,56 +97,62 @@ exports.channel = async (req, res) => {
 };
 
 exports.store = async (req, res) => {
-  var feedItem = FeedItems(req.body);
+  const feedItem = new FeedItems(req.body);
   feedItem.active = true;
+  if (req.files) {
+    const images = await upload(req, {
+      folder: 'feed_images',
+      format: 'jpg',
+    });
+    feedItem.images = images;
+  }
   Channels.findById(feedItem.channel, (error, channel) => {
-    if(error) {
+    if (error) {
       res.send(error);
     }
-    var userHasPermission = channel.users.some((user) => user.canCreate && String(user._id) == String(req.user._id));
-    if(!userHasPermission) {
-      res.status(401).send("You do not have permission to post to this news feed channel!");
+    const userHasPermission = channel.users.some((user) => user.canCreate && String(user._id) == String(req.user._id));
+    if (!userHasPermission) {
+      res.status(401).send('You do not have permission to post to this news feed channel!');
       return;
     }
     feedItem.save((error, item) => {
       error ? res.status(501).send({ error }) : res.send(item);
-      if(feedItem.push) {
-        //send a push notification here
-        //need to translate feedItem into a Notification object first
-        //TODO? currently, this means that the Notification form will be sent back, not the feedItem.
+      if (feedItem.push) {
+        // send a push notification here
+        // need to translate feedItem into a Notification object first
+        // TODO? currently, this means that the Notification form will be sent back, not the feedItem.
         PushHandler.sendPost(feedItem, channel)
-        .then(function(results) {
-          feeditems_cache.force_reload();
-        }).catch(function(error) {
-          //TODO: returning an error would be cleaner
-          console.log("error 2: ", error);
-          res
-            .status(501)
-            .send({ error: `Error fetching push tokens: ${error}` });
-          return;
-        });
+          .then((results) => {
+            feeditems_cache.force_reload();
+          }).catch((error) => {
+          // TODO: returning an error would be cleaner
+            console.log('error 2: ', error);
+            res
+              .status(501)
+              .send({ error: `Error fetching push tokens: ${error}` });
+          });
       } else {
         feeditems_cache.force_reload();
       }
     });
-  }
-)};
+  });
+};
 
 exports.activate = async (req, res) => {
-  FeedItems.update({_id: req.params.id}, {
-    active: true
-  }, 
-  function(err, affected, resp) {
+  FeedItems.update({ _id: req.params.id }, {
+    active: true,
+  },
+  (err, affected, resp) => {
     res.send(resp);
     feeditems_cache.force_reload();
   });
 };
 
 exports.deactivate = async (req, res) => {
-  FeedItems.update({_id: req.params.id}, {
-    active: false
-  }, 
-  function(err, affected, resp) {
+  FeedItems.update({ _id: req.params.id }, {
+    active: false,
+  },
+  (err, affected, resp) => {
     res.send(resp);
     feeditems_cache.force_reload();
   });
@@ -150,21 +160,21 @@ exports.deactivate = async (req, res) => {
 
 exports.delete = (req, res) => {
   FeedItems.findById(req.params.id, (error, feedItem) => {
-    if(error) res.status(501).send({error});
+    if (error) res.status(501).send({ error });
 
     Channels.findById(feedItem.channel.Id), (error, channel) => {
-      if(error) {
+      if (error) {
         res.send(error);
       }
-      var userHasPermission = channel.users.some((user) => user.canDelete && user._id === req.user._id);
-      if(!userHasPermission) {
-        res.status(401).send("You do not have permission to delete from this news feed channel!");
+      const userHasPermission = channel.users.some((user) => user.canDelete && user._id === req.user._id);
+      if (!userHasPermission) {
+        res.status(401).send('You do not have permission to delete from this news feed channel!');
       }
-      FeedItems.findByIdAndRemove(req.params.id, error => {
+      FeedItems.findByIdAndRemove(req.params.id, (error) => {
         error
           ? res.status(501).send({ error })
-          : res.send({ message: "Deleted" + req.params.id });
+          : res.send({ message: `Deleted${req.params.id}` });
       });
-    }
+    };
   });
 };
