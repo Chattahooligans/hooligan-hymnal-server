7e97b2ba7280274b02c2e51a9d0a5b428f31ac05
Collin OConnell <ococncol@gmail.com>
2020-02-11 08:57:06 -0500

    secret

--- handlers/imageUploader.js
+++ handlers/imageUploader.js
@@ -11,6 +11,10 @@ exports.upload = async (req, options) => {
   for (const file in files) {
     if (files.hasOwnProperty(file)) {
       const path = files[file].tempFilePath;
+      // TODO: See if we can upload the images based on type to cloudinary
+      // if (!options.type) {
+      //   options.type = files[file].type;
+      // }
       const image = await cloudinary.uploader.upload(path, options);
       images = [...images, image];
     }
