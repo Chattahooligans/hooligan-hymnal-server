b60f285d7a6d21497413ff59d929c03754902f51
Galen Riley <galenriley@gmail.com>
2020-02-19 18:17:13 -0500

    remove server transform

--- controllers/API/feedController.js
+++ controllers/API/feedController.js
@@ -111,10 +111,7 @@ exports.store = async (req, res) => {
     const targetFolder = 'feed';
 
     const images = await upload(req, {
-      folder: 'feed',
-      transformation: {
-        width: 700,
-      },
+      folder: 'feed'
     });
     if (Array.isArray(images)) {
       images.map((image, index) => req.body.images.push({ url: image.url, uri: image.url, metadata: JSON.parse(req.body.metadata[index]) }));
