6c9e7af5b85f2914370f0083dac43279724cd2df
Collin <ococncol@gmail.com>
2020-03-04 00:27:51 -0500

    still remaking form

--- src/js/react.js
+++ src/js/react.js
@@ -1,13 +1,38 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+// import { Formik, Form, Field } from 'formik';
+// import * as Yup from 'yup';
 
 const SongbookForm = () => {
-  const [songbook, setSongbook] = useState({});
+  const [songbook, setSongbook] = useState({
+    songbook_title: '',
+    organization: '',
+    description: '',
+    front_cover: '',
+    back_cover: '',
+    chapters: [],
+  });
+  const [chapters, setChapters] = useState([]);
+  const [newChapter, setNewChapter] = useState({
+    chapter_title: '',
+  });
+  const [newSongs, setSongs] = useState({
+    featured: false,
+    hint: '',
+  });
+
   const handleInput = ({ target: { name, value } }) => {
-    console.log(name, value);
+    setSongbook({
+      ...songbook,
+      [name]: value,
+    });
+  };
+
+  const updateChapterTitle = ({ target: { name, value } }) => {
+    setNewChapter({
+      chapter_title: value,
+    });
   };
   return (
     <>
@@ -15,13 +40,47 @@ const SongbookForm = () => {
         <div className="flex flex-col mb-3">
           <label htmlFor="name">
             Name
-            <input type="text" name="name" id="name" className="border block" />
+            <input type="text" name="name" id="name" className="border block" onChange={handleInput} value={songbook.name} />
           </label>
         </div>
       </form>
+      <div>
+        <input type="text" name="chapters" id="chapters" className="border block" value={newChapter.chapter_title} onChange={updateChapterTitle} />
+        <button
+          type="button"
+          onClick={(e) => {
+            setSongbook({
+              ...songbook,
+              chapters: [
+                ...songbook.chapters,
+                {
+                  chapter_title: newChapter.chapter_title,
+                  songs: [],
+                },
+              ],
+            });
+            setNewChapter({
+              chapter_title: '',
+            });
+          }}
+        >
+          &plus;
+
+        </button>
+      </div>
+      <ul>
+        {songbook.chapters.map((chapter, index) => (
+          <li key={index}>
+            {chapter.chapter_title}
+            <div>
+              <input type="text" id="hint" name="hint" />
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<SongbookForm />, document.querySelector('#songbookForm'));
+render(<SongbookForm />, document.querySelector('#songbookForm'));
