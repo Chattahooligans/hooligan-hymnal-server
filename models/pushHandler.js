bef086b7a5c5b85ace97afa9b348f4898a7617ec
Collin <ococncol@gmail.com>
2020-03-04 23:15:03 -0500

    push now returns receipts and errors.. also deletes tokens out of db from other apps

--- models/pushHandler.js
+++ models/pushHandler.js
@@ -3,171 +3,89 @@ const PushTokens = require('./pushTokens');
 
 const expo = new Expo();
 
-async function sendPush(push) {
+async function sendPush(feedItem, senderToken, channel) {
   console.log('sending push...');
-  const tokens = await PushTokens.find();
+  const pushTokens = await PushTokens.find();
   const messages = [];
-  const errors = [];
-  let receipts = [];
-  tokens.map((pushToken) => {
-    console.log(pushToken);
-    if (!Expo.isExpoPushToken(pushToken.pushToken)) {
-      console.error(`Push token ${pushToken.pushToken} is not a valid expo push token`);
-    }
-    return messages.push({
-      to: pushToken.pushToken,
-      sound: 'default',
-      title: push.title,
-      body: push.body,
-      data: push.data,
-    });
-  });
-  const chunks = expo.chunkPushNotifications(messages);
   const tickets = [];
-  (async () => {
-    chunks.map(async (chunk) => {
-      try {
-        const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-        console.log(ticketChunk);
-        tickets.push(...ticketChunk);
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  })();
-
+  const errors = [];
+  pushTokens.map(async (pushToken) => {
+    if (!Expo.isExpoPushToken(pushToken.pushToken)) {
+      console.error(`Push token ${pushToken.pushToken} is not valid`);
+    }
+    if (pushToken.expoExperience === senderToken.expoExperience) {
+      messages.push({
+        to: pushToken.pushToken,
+        sound: 'default',
+        title: `New notification from ${channel.name}`,
+        body: `${feedItem.text}... (tap to view more)`,
+        data: { post: feedItem.id },
+      });
+    }
+  });
+  // The Expo push notification service accepts batches of notifications so
+  // that you don't need to send 1000 requests to send 1000 notifications. We
+  // recommend you batch your notifications to reduce the number of requests
+  // and to compress them (notifications with similar content will get
+  // compressed).
+  const chunks = expo.chunkPushNotifications(messages);
+  // Send the chunks to the Expo push notification service. There are
+  // different strategies you could use. A simple one is to send one chunk at a
+  // time, which nicely spreads the load out over time:
+  for (const chunk of chunks) {
+    try {
+      const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      tickets.push(...ticketChunk);
+      // NOTE: If a ticket contains an error code in ticket.details.error, you
+      // must handle it appropriately. The error codes are listed in the Expo
+      // documentation:
+      // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+    } catch (error) {
+      console.error(error);
+    }
+  }
   const receiptIds = [];
   for (const ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
     if (ticket.id) {
       receiptIds.push(ticket.id);
     }
   }
 
   const receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-  (async () => {
-    for (const chunk of receiptIdChunks) {
-      try {
-        receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-        console.log(receipts);
-        for (const receiptId in receipts) {
-          const { status, message, details } = receipts[receiptId];
-          if (status === 'ok') {
-            continue;
-          } else if (status === 'error') {
-            errors.push(message);
-            console.error(
-              `There was an error sending a notification: ${message}`,
-            );
-            if (details && details.error) {
-              console.error(`The error code is ${details.error}`);
-            }
+  // Like sending notifications, there are different strategies you could use
+  // to retrieve batches of receipts from the Expo service.
+  for (const chunk of receiptIdChunks) {
+    try {
+      const receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+      // The receipts specify whether Apple or Google successfully received the
+      // notification and information about an error, if one occurred.
+      for (const receiptId in receipts) {
+        const { status, message, details } = receipts[receiptId];
+        if (status === 'ok') {
+          continue;
+        } else if (status === 'error') {
+          errors.push({ status, details });
+          console.error(
+            `There was an error sending a notification: ${message}`,
+          );
+          if (details && details.error) {
+            // The error codes are listed in the Expo documentation:
+            // https://docs.expo.io/versions/latest/guides/push-notifications/#individual-errors
+            // You must handle the errors appropriately.
+            console.error(`The error code is ${details.error}`);
           }
         }
-      } catch (error) {
-        console.error(error);
       }
+    } catch (error) {
+      console.error(error);
     }
-  })();
-  // const chunks = expo.chunkPushNotifications(tokens);
-  // chunks.forEach(async (chunk) => {
-  //   const notifications = await chunk.map((token) => {
-  //     // console.log(`trying to send notification to token: ${token.pushToken}`);
-  //     return {
-  //       to: token.pushToken,
-  //       sound: 'default',
-  //       title: push.title,
-  //       body: push.body,
-  //       data: push.data,
-  //     };
-  //   });
-  // });
-
-  // PushTokens.find(async (error, tokens) => {
-  //   if (error) {
-  //     throw error;
-  //   }
-  //   // const errors = [];
-  //   // const receipts = [];
-  //   const chunks = expo.chunkPushNotifications(tokens);
-  //   for (chunk of chunks) {
-  //     const notifications = chunk.map((token) => {
-  //       console.log(
-  //         'trying to send notification to token: ',
-  //         token.pushToken,
-  //       );
-  //       return {
-  //         to: token.pushToken,
-  //         sound: 'default',
-  //         title: push.title,
-  //         body: push.body,
-  //         data: push.data,
-  //       };
-  //     });
-  //     try {
-  //       console.log('trying to push');
-  //       receipts.push(
-  //         ...(await expo.sendPushNotificationsAsync(notifications)),
-  //       );
-  //     } catch (error) {
-  //       console.log('there was a problem with the push');
-  //       const tokenString = chunk
-  //         .map((token) => token.pushToken)
-  //         .join(', ');
-  //       console.error(
-  //         `Error notifying with tokens [${tokenString}]: ${error}`,
-  //       );
-
-  //       /*
-  // 				This line returns an object like:
-  // 				{
-  // 					"@chattahooligan/chattahooligan-hymnal":
-  // 						["ExponentPushToken[NQjQBqBjcnAfQk8N-3R2al]","ExponentPushToken[HcDpdlINasiVlvc8fOIlEa]","ExponentPushToken[I8QML2Ir_01kPeOXEfjizU]","ExponentPushToken[jqoXnuCi8Acw2sk9Dtq_C2]"],
-  // 					"@ngsdetroit/chattahooligan-hymnal":
-  // 						["ExponentPushToken[rA-sHTCBP85w2U3D9kix17]","ExponentPushToken[r76-uHJwwHZM8y5OwgXNOW]"]
-  // 				}
-
-  // 				suggest:
-  // 				- an environment variable of the acceptable expo experience
-  // 				- not register tokens that don't match
-  // 				- capturing keys in that error.details that don't match, deleting associated tokens
-  // 			*/
-  //       console.error(
-  //         `Details: ${JSON.stringify(error.details)}`,
-  //       );
-  //       errors.push(
-  //         ...chunk.map(
-  //           (token) => `Error notifying with token ${token}: ${error}`,
-  //         ),
-  //       );
-  //     }
-  //   }
-  //   console.log(`Starting tokenMatcher block, receipts.length: ${receipts.length}`);
-  //   const tokenMatcher = new RegExp('ExponentPushToken');
-  //   receipts.forEach((receipt) => {
-  //     console.log(`iterating on ${JSON.stringify(receipt)}`);
-  //     if (receipt.status == 'error') {
-  //       console.log(`error found on ${receipt.message}`);
-  //       // run regex to retrieve token from it
-  //       const matches = tokenMatcher.exec(receipt.message);
-  //       if (matches.length > 0) {
-  //         const i = matches.index;
-  //         const token = receipt.message.substring(i, i + 41);
-  //         // if token found, find and delete
-  //         PushTokens.deleteOne({ pushToken: token }).then(
-  //           (deleteResult) => {
-  //             console.log('deleted bad push token');
-  //           },
-  //         );
-  //       }
-  //     }
-  //   });
-  //   res.send({ errors, receipts });
-  // });
+  }
   return {
+    tickets,
     errors,
-    receipts,
   };
-  // return res.send({ errors, receipts });
 }
 
 async function sendNotification(notification) {
@@ -194,4 +112,4 @@ async function sendPost(post, channel) {
   await sendPush(push);
 }
 
-module.exports = { sendNotification, sendPost };
+module.exports = { sendNotification, sendPost, sendPush };
