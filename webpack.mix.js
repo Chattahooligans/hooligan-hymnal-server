fd4b6d0b7f4796f80225a96b4c50991c5f9428b4
Collin <ococncol@gmail.com>
2020-03-12 23:58:59 -0400

    working on the songform

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,6 @@ const tailwindcss = require('tailwindcss');
 
 mix
   .js('src/js/app.js', 'public/js')
-  .react('src/js/react.js', 'public/js')
   .sass('src/scss/app.scss', 'public/css')
   .options({
     processCssUrls: false,
