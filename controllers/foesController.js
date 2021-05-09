e79621db24127751233aa8b935be0c8aabac3358
shortnd <ococncol@gmail.com>
2021-05-09 10:47:13 -0400

    moved csv upload to own function and updated form

--- controllers/foesController.js
+++ controllers/foesController.js
@@ -10,6 +10,33 @@ const { deleteCache } = require('../middleware/cacheMiddleware');
 
 const DELETE_FOES_CACHE = () => deleteCache('foes');
 
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+ async function foesCsvParser(req, res, foe) {
+  const players = []
+  if (req.files && req.files.foeCSV) {
+    const file = req.files.foeCSV
+    if (['application/vnd.ms-excel','text/plain','text/csv','text/tsv'].indexOf(file.mimetype) === -1) {
+      req.flash('error', 'Please upload a CSV file')
+      return res.redirect((typeof foe !== 'undefined') ? `/foe/${foe.id}` : `/foes/create`)
+    }
+    const parser = fs.createReadStream(file.tempFilePath).pipe(csv())
+    for await (const record of parser) {
+      if (record.Name !== '') {
+        players.push({
+          name: record.Name,
+          position: record.Position,
+          number: record.Number
+        })
+      }
+    }
+  }
+  return players
+}
+
 exports.index = async (req, res) => {
   const { all } = req.query;
   let foes = await Foe.find({});
@@ -37,30 +64,15 @@ exports.create = (req, res) => {
  * @param {import('express').Response} res
  */
 exports.store = async (req, res) => {
-  const players = [];
-  if (req.files && req.files.foeCSV) {
-    const file = req.files.foeCSV
-    // if (file.mimetype !== 'text/csv' || file.mimetype !== 'application/vnd.ms-excel') {
-    //   console.log(file.mimetype)
-    //   req.flash('error', 'Please upload a CSV file')
-    //   return res.redirect(`/foes/create`)
-    // }
-    const parser = fs.createReadStream(file.tempFilePath).pipe(csv())
-    for await (const record of parser) {
-      if (record.Name !== '') {
-        players.push({
-          name: record.Name,
-          position: record.Position,
-          number: record.Number
-        })
-      }
-    }
-    req.body.players = players
-  } else if (req.body.players) {
-    req.body.players.map((player) => {
+  let players = [];
+  players = await foesCsvParser(req, res)
+  if (req.body['players[]']) {
+    req.body['players[]'].map((player) => {
       players.push(JSON.parse(player));
     });
     req.body.players = players;
+  } else {
+    req.body.players = players
   }
   if (req.body.active) {
     req.body.active = true;
@@ -87,27 +99,18 @@ exports.edit = async (req, res) => {
   });
 };
 
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
 exports.update = async (req, res) => {
-  const players = [];
-  if (req.files && req.files.foeCSV) {
-    const file = req.files.foeCSV
-    // if (file.mimetype !== 'text/csv') {
-    //   req.flash('error', 'Please upload a CSV file')
-    //   return res.redirect(`/foes/create`)
-    // }
-    const parser = fs.createReadStream(file.tempFilePath).pipe(csv())
-    for await (const record of parser) {
-      if (record.Name !== '') {
-        players.push({
-          name: record.Name,
-          position: record.Position,
-          number: record.Number
-        })
-      }
-    }
-    req.body.players = players
-  } else if (req.body.players) {
-    req.body.players.map((player) => players.push(JSON.parse(player)));
+  let players = [];
+  let foe = await Foe.findById(req.params.id)
+  players = await foesCsvParser(req, res, foe)
+  console.log(req.body)
+  if (req.body['players[]']) {
+    req.body['players[]'].map((player) => players.push(JSON.parse(player)));
     req.body.players = players;
   } else {
     req.body.players = players;
@@ -121,7 +124,7 @@ exports.update = async (req, res) => {
   } else {
     req.body.active = true;
   }
-  const foe = await Foe.findOneAndUpdate(
+  foe = await Foe.findOneAndUpdate(
     {
       _id: req.params.id,
     },
